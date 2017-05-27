4b586890370891fab0f675817fb536de7fd99ca7
Terseus <terseus@gmail.com>
2017-05-27 12:08:08 +0200

    Add packaging support

---
+++ io_extra/_io_extra_meta.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+
+NAME = "io-extra"
+VERSION = (0, 1, 0)
+VERSION_STR = '.'.join(map(str, VERSION))
+LICENSE = "MIT"
+DESCRIPTION = "Extra utilities to work with the io module"
+AUTHOR = "David Caro"
+AUTHOR_EMAIL = "terseus@gmail.com"
+KEYWORDS = ["io", "library"]
+CLASSIFIERS = [
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Natural Language :: English",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
---
+++ setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+import sys
+
+
+HERE = path.abspath(path.dirname(__file__))
+sys.path.append(path.join(HERE, 'io_extra'))
+
+
+import _io_extra_meta as app_meta  # noqa
+
+
+# Get the long description from the README file
+def get_long_description():
+    with open(path.join(HERE, 'README.rst'), encoding='utf-8') as f:
+        return f.read()
+
+
+setup(
+    name=app_meta.NAME,
+    version=app_meta.VERSION_STR,
+    author=app_meta.AUTHOR,
+    author_email=app_meta.AUTHOR_EMAIL,
+    license=app_meta.LICENSE,
+    classifiers=app_meta.CLASSIFIERS,
+    keywords=app_meta.KEYWORDS,
+    packages=find_packages(exclude=['docs', 'tests', 'venv']),
+)
