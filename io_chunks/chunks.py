68fb460769d4af9ebe3d1381a547042def1d7b53
Terseus <terseus@gmail.com>
2017-06-03 11:19:50 +0200

    Oops, reverse super arguments

--- io_chunks/chunks.py
+++ io_chunks/chunks.py
@@ -50,7 +50,7 @@ class RawIOChunk(RawIOBase):
         self._size = size
         self._cursor = 0
         self._stream = stream
-        super(self, RawIOChunk).__init__()
+        super(RawIOChunk, self).__init__()
 
     def readinto(self, array):
         if not isinstance(array, (bytearray, memoryview)):
