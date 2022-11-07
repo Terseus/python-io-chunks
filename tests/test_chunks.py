fad49fa5fe5ccf4d070f2fb370bada5b1367faea
Terseus <terseus@fastmail.com>
2022-11-07 23:19:49 +0100

    Rewrite the tests
    Better as independent functions that in a class; some bugs appeared.

--- tests/test_chunks.py
+++ tests/test_chunks.py
@@ -1,75 +1,74 @@
-import unittest
 from io import SEEK_CUR, SEEK_END, BytesIO
+
+import pytest
 
 from io_chunks import RawIOChunk
 
 
-class TestRawIOChunk(unittest.TestCase):
-    STRINGS = [
-        b"hello",
-        b"beautiful",
-        b"world",
-    ]
+def test_file_begin():
+    with BytesIO(b"0123456789") as buffer:
+        chunk = RawIOChunk(buffer, size=5)
+        assert chunk.start == 0
+        assert chunk.size == 5
+        assert chunk.end == 5
+        assert chunk.tell() == 0
+        assert chunk.read() == b"01234"
+        assert chunk.tell() == 5
 
-    def get_raw_buffer(self):
-        return BytesIO(b"".join(self.STRINGS))
 
-    def test_file_begin(self):
-        with self.get_raw_buffer() as data_file:
-            chunk = RawIOChunk(data_file, len(self.STRINGS[0]))
-            self.assertEqual(chunk.start, 0)
-            self.assertEqual(chunk.size, len(self.STRINGS[0]))
-            self.assertEqual(chunk.end, chunk.start + chunk.size)
-            self.assertEqual(chunk.tell(), chunk.start)
-            self.assertEqual(chunk.read(), self.STRINGS[0])
-            self.assertEqual(chunk.tell(), chunk.end)
+def test_file_middle():
+    with BytesIO(b"0123456789") as buffer:
+        chunk = RawIOChunk(buffer, size=5, start=2)
+        assert chunk.read() == b"23456"
 
-    def test_file_middle(self):
-        with self.get_raw_buffer() as data_file:
-            chunk = RawIOChunk(data_file, len(self.STRINGS[1]), len(self.STRINGS[0]))
-            self.assertEqual(chunk.read(), self.STRINGS[1])
 
-    def test_eof(self):
-        with self.get_raw_buffer() as data_file:
-            chunk = RawIOChunk(
-                data_file,
-                len(self.STRINGS[2]) + 1,
-                len(self.STRINGS[0]) + len(self.STRINGS[1]),
-            )
-            with self.assertRaises(EOFError):
-                chunk.read()
-            chunk.seek(0)
-            self.assertEqual(chunk.read(chunk.size - 1), self.STRINGS[2])
-            with self.assertRaises(EOFError):
-                chunk.read(1)
+@pytest.mark.xfail(strict=True, reason="Bug or feature?")
+def test_read_until_eof():
+    with BytesIO(b"0123456789") as buffer:
+        chunk = RawIOChunk(buffer, size=6, start=5)
+        assert chunk.read() == b"56789"
 
-    def test_closed(self):
-        with self.get_raw_buffer() as data_file:
-            chunk = RawIOChunk(data_file, len(self.STRINGS[0]))
-            self.assertFalse(chunk.closed)
-        self.assertTrue(chunk.closed)
-        with self.assertRaises(ValueError):
+
+@pytest.mark.xfail(strict=True, reason="Known bug - remaining is zero")
+def test_read_past_eof():
+    with BytesIO(b"0123456789") as buffer:
+        chunk = RawIOChunk(buffer, size=1, start=9)
+        assert chunk.read(1) == b"9"
+        with pytest.raises(EOFError):
             chunk.read()
 
-    def test_seek(self):
-        with self.get_raw_buffer() as data_file:
-            chunk = RawIOChunk(data_file, len(self.STRINGS[1]), len(self.STRINGS[0]))
-            self.assertEqual(chunk.seek(1), 1)
-            self.assertEqual(chunk.tell(), 1)
-            self.assertEqual(chunk.seek(1, SEEK_CUR), 2)
-            self.assertEqual(chunk.tell(), 2)
-            self.assertEqual(chunk.seek(-1, SEEK_END), len(self.STRINGS[1]) - 1)
-            self.assertEqual(chunk.tell(), len(self.STRINGS[1]) - 1)
-            self.assertEqual(chunk.seek(0), 0)
-            self.assertEqual(chunk.read(), self.STRINGS[1])
-            self.assertEqual(chunk.seek(0), 0)
-            self.assertEqual(chunk.read(), self.STRINGS[1])
 
-    def test_full_file(self):
-        with self.get_raw_buffer() as data_file:
-            chunks = [RawIOChunk(data_file, len(self.STRINGS[0]))]
-            chunks.append(RawIOChunk(data_file, len(self.STRINGS[1]), chunks[0].end))
-            chunks.append(RawIOChunk(data_file, len(self.STRINGS[2]), chunks[1].end))
-            self.assertEqual(
-                sum(chunk.size for chunk in chunks), len(b"".join(self.STRINGS))
-            )
+def test_eof():
+    with BytesIO(b"0123456789") as buffer:
+        chunk = RawIOChunk(buffer, 6, 5)
+        with pytest.raises(EOFError):
+            chunk.read()
+        chunk.seek(0)
+        assert chunk.read(5) == b"56789"
+        with pytest.raises(EOFError):
+            # FIXME: Here remaining is 1, why?
+            chunk.read(1)
+
+
+def test_closed():
+    with BytesIO(b"01234") as buffer:
+        chunk = RawIOChunk(buffer, size=5)
+        assert chunk.closed is False
+    assert chunk.closed is True
+    with pytest.raises(ValueError):
+        chunk.read()
+
+
+def test_seek():
+    with BytesIO(b"0123456789") as buffer:
+        chunk = RawIOChunk(buffer, size=5, start=5)
+        assert chunk.tell() == 0
+        assert chunk.seek(1) == 1
+        assert chunk.tell() == 1
+        assert chunk.seek(1, SEEK_CUR) == 2
+        assert chunk.tell() == 2
+        assert chunk.seek(-1, SEEK_END) == 4
+        assert chunk.seek(2) == 2
+        assert chunk.read() == b"789"
+        assert chunk.seek(0) == 0
+        assert chunk.read() == b"56789"
